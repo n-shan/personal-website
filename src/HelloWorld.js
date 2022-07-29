07ccf15d4fc133f4b74b010f138b586db2f8e6f4
Nikhil Shan <47575072+n-shan@users.noreply.github.com>
2022-07-28 23:04:11 -0700

    add hello world

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+const HelloWorld = () => {
+  
+  function sayHello() {
+    alert('Hello, World!');
+  }
+  
+  return (
+    <button onClick={sayHello}>Click me!</button>
+  );
+};
+
+export default HelloWorld;
